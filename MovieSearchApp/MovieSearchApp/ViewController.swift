09ae88356e56f69f4c975d065504d7f766a8b9fb
Lahiru Chathuranga <hiru.wlc@gmail.com>
2024-10-30 23:33:13 +0800

    Initial commit with project setup

---
+++ MovieSearchApp/MovieSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieSearchApp
+//
+//  Created by Lahiru Chathuranga on 2024-10-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
